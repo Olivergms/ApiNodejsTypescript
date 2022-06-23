5d91f328e3cded999851d28a6bdf5a67bab2dc42
Adriano Gomes de Oliveira <oliveradriano.ads@gmail.com>
2022-06-23 19:14:21 -0300

    criação de servico, rota e repositorio de Specification

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,12 @@
+import { Specification } from "../model/Specification";
+
+interface ICreateSpecificationDto{
+    name: string;
+    description: string;
+}
+interface ISpecificationsRepository{
+    create( {name, description} : ICreateSpecificationDto) : void;
+    findByName(name: string) : Specification;
+}
+
+export {ISpecificationsRepository, ICreateSpecificationDto}
